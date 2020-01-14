589257eb849460680606ad322ac8f25ff7dcd731
Emir Smajlovic <emir.student.smajlovic@gmail.com>
2020-01-14 21:34:02 +0100

    Initial commit

---
+++ SimplexAlogritam.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SimplexAlogritam {
+
+	public static void main(String[] args) {
+		Scanner unos = new Scanner(System.in);
+		
+		// funkcija cilja
+		System.out.println("Unesite funkciju cilja: ");
+		List<Integer> funkcijaCilja = new ArrayList<Integer>();
+		do {
+			funkcijaCilja.add((unos.nextInt()));
+		} while (unos.nextInt() != -1);
+		
+		// ogranicenja
+		
+		System.out.println("Unesite broj ogranicenja:");
+		int brojRedova = unos.nextInt();
+		System.out.println("Unesite broj varijabli: ");
+		int brojKolona = unos.nextInt();
+		System.out.println("Unesite koeficijente uz ogranicenja u vasu matricu: ");
+		double[][] matricaOgranicenja = new double[brojRedova][brojKolona];
+		for (int i = 0; i < brojRedova; i++) {
+			for (int j = 0; j < brojKolona; j++) {
+				matricaOgranicenja[i][j] = unos.nextDouble();
+			}
+		}
+		for (int i = 0; i < brojRedova; i++) {
+			for (int j = 0; j < brojKolona; j++) {
+				System.out.print(matricaOgranicenja[i][j] + " ");
+				if (j == brojKolona - 1) {System.out.println();}
+			}
+		}
+		unos.close();
+	}
+
+}
---
+++ SimplexVarijable.java
@@ -0,0 +1,7 @@
+
+public class SimplexVarijable {
+	double vrijednostVarijable;
+	SimplexVarijable () {
+		
+	}
+}
